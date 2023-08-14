75bed3aa6818766f5a72b0381a2ed49ef1503e62
sergio santana <sergiosantana.m@gmail.com>
2023-08-14 15:26:12 -0400

    stupid coach finish

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,15 +1,18 @@
+
+
 class QuestionsController < ApplicationController
+  def ask; end
 
-  def ask;
-  end
   def answer
-   @answer = ['Great!', 'Silly question, get dressed and go to work!', "I don't care, get dressed and go to work!"]
-   query = params[:question]
-   if
-
-
-
-
-   end
+    @answers = ['Great!', 'Silly question, get dressed and go to work!', "I don't care, get dressed and go to work!"]
+    query = params[:question]
+    # raise
+  if query == 'I am going to work'
+    @answer = 'Great!'
+  elsif query.end_with?('?')
+    @answer = 'Silly question, get dressed and go to work!.'
+  else
+    @answer = "I don't care, get dressed and go to work!"
+  end
   end
 end
